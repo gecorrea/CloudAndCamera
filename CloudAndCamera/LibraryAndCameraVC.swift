355e132db92019d3c5a7d68de69e73bcb31b5c2d
George <george.e.correa@gmail.com>
2017-06-09 16:27:18 -0400

    Now have access to device camera and photo library with user consent.

--- CloudAndCamera/LibraryAndCameraVC.swift
+++ CloudAndCamera/LibraryAndCameraVC.swift
@@ -1,30 +1,59 @@
 import UIKit
+import MobileCoreServices
 
-class LibraryAndCameraVC: UIViewController {
+class LibraryAndCameraVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var libraryButton: UIButton!
+    let picker = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        picker.delegate = self
+        picker.mediaTypes = [kUTTypeImage as String]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+//        imageView.contentMode = .scaleAspectFit
+//        imageView.image = chosenImage
+//        imageView.isHidden = false
+//        shareButton.isEnabled = true
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    @IBAction func cameraButtonPushed(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker,animated: true,completion: nil)
+        }
+        else {
+            noCamera()
+        }
+    }
+    
+    func noCamera(){
+        let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style:.default, handler: nil)
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
+    }
+
+    @IBAction func libraryButtonPushed(_ sender: UIButton) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
 
 }
