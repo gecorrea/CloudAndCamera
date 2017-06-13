0935970c19835faed175a2f3ab5f06faf017aa6d
George <george.e.correa@gmail.com>
2017-06-13 13:08:57 -0400

    Group storyboards and view controllers into group files. Add UI and UX features to sign in and sign up VCs.

--- CloudAndCamera/SignInVC.swift
+++ CloudAndCamera/SignInVC.swift
@@ -2,14 +2,28 @@ import UIKit
 
 class SignInVC: UIViewController {
     
-    @IBOutlet weak var cloudLabel: UILabel!
-    @IBOutlet weak var andLabel: UILabel!
-    @IBOutlet weak var cameraLabel: UILabel!
-    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.backgroundColor = UIColor.clear
+        emailTextField.tintColor = UIColor.white
+        emailTextField.textColor = UIColor.white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let emailBottomLayer = CALayer()
+        emailBottomLayer.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        emailBottomLayer.backgroundColor = UIColor.white.cgColor
+        emailTextField.layer.addSublayer(emailBottomLayer)
 
+        passwordTextField.backgroundColor = UIColor.clear
+        passwordTextField.tintColor = UIColor.white
+        passwordTextField.textColor = UIColor.white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let passwordBottomLayer = CALayer()
+        passwordBottomLayer.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        passwordBottomLayer.backgroundColor = UIColor.white.cgColor
+        passwordTextField.layer.addSublayer(passwordBottomLayer)
     }
 
 
--- CloudAndCamera/SignUpVC.swift
+++ CloudAndCamera/SignUpVC.swift
@@ -1,16 +1,48 @@
 import UIKit
 
 class SignUpVC: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userNameTextField.backgroundColor = UIColor.clear
+        userNameTextField.tintColor = UIColor.white
+        userNameTextField.textColor = UIColor.white
+        userNameTextField.attributedPlaceholder = NSAttributedString(string: userNameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let userNameBottomLayer = CALayer()
+        userNameBottomLayer.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        userNameBottomLayer.backgroundColor = UIColor.white.cgColor
+        userNameTextField.layer.addSublayer(userNameBottomLayer)
 
-        // Do any additional setup after loading the view.
+        emailTextField.backgroundColor = UIColor.clear
+        emailTextField.tintColor = UIColor.white
+        emailTextField.textColor = UIColor.white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let emailBottomLayer = CALayer()
+        emailBottomLayer.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        emailBottomLayer.backgroundColor = UIColor.white.cgColor
+        emailTextField.layer.addSublayer(emailBottomLayer)
+        
+        passwordTextField.backgroundColor = UIColor.clear
+        passwordTextField.tintColor = UIColor.white
+        passwordTextField.textColor = UIColor.white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor(white: 1.0, alpha: 0.6)])
+        let passwordBottomLayer = CALayer()
+        passwordBottomLayer.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        passwordBottomLayer.backgroundColor = UIColor.white.cgColor
+        passwordTextField.layer.addSublayer(passwordBottomLayer)
+        
+        profileImage.layer.cornerRadius = profileImage.frame.size.height/2
+        profileImage.clipsToBounds = true
     }
 
-    @IBAction func dismissSignUp(_ sender: UIButton) {
+    @IBAction func alreadyUser(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-
-
 }
